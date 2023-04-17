6af8ccc2237895f4a2b48b83bb842facf75da345
simoneOT <tapilyousmane3@gmail.com>
2023-04-17 18:04:25 +0000

    navbar

--- App.js
+++ App.js
@@ -1,15 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
+import { Button, StyleSheet, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import View1 from './Component/View1';
 import View2 from './Component/View2';
 
+const Stack = createNativeStackNavigator();
 export default function App() {
   
   return (
-    <View style={styles.container}>
-        <View1/>
-        {/* <View2/> */}
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="View1" component={View1}
+          options={({ navigation, route }) => ({
+            headerRight: () => (
+              <Button title="Aller vers" 
+                onPress={()=>navigation.navigate("View2")}
+              />
+            ),
+          })}
+        />
+        <Stack.Screen name="View2" component={View2} />
+      </Stack.Navigator>
+  </NavigationContainer>
   );
 }
 
